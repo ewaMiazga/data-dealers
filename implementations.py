ce0b513abb5f8c95002f7752e8f525975e8b9ac2
elboyer <elise.boyer@epfl.ch>
2024-09-30 18:02:21 +0200

    least squares function

--- implementations.py
+++ implementations.py
@@ -176,10 +176,25 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     return w, loss
 
 def least_squares(y, tx):
-    # ***************************************************
-    # TODO: Elise implementation of least squares
-    # ***************************************************
-    raise NotImplementedError
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
+
+    w = np.linalg.solve(tx.T.dot(tx), tx.T.dot(y))
+    mse = compute_loss(y, tx, w)
+
+    return w, mse 
 
 def ridge_regression(y, tx, lambda_):
     # ***************************************************
